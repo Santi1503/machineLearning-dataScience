e898cf56c79e346f9b543ed67edab011aeebcd4b
Santi1503 <gomezdelatorre15@gmail.com>
2024-05-03 14:24:02 +0200

    Adding

---
+++ LinearRegression.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+calculoEquipos = 1500
+
+
+x = 2 * np.random.rand(100,1)
+y = 4 + 3 * x + np.random.randn(100,1)
+
+print("La longitud del conjunto es: ", len(x))
+
+
+data = {'equiposAfectados': x.flatten(), 'costeIncidente': y.flatten()}
+df = pd.DataFrame(data)
+df.head(10)
+
+df['equiposAfectados'] = df['equiposAfectados'] * 1000
+df['equiposAfectados'] = df['equiposAfectados'].astype(int)
+df['costeIncidente'] = df['costeIncidente'] * 10000
+df['equiposAfectados'] = df['equiposAfectados'].astype(int)
+df.head(10)
+
+linReg = LinearRegression()
+linReg.fit(df['equiposAfectados'].values.reshape(-1,1), df['costeIncidente'].values)
+
+print("El coeficiente de la regresión lineal es: ", linReg.coef_)
+print("El intercepto de la regresión lineal es: ", linReg.intercept_)
+
+xMinMax = np.array([[df['equiposAfectados'].min()], [df['equiposAfectados'].max()]])
+yTrainPred = linReg.predict(xMinMax)
+
+xNew = np.array([[calculoEquipos]])
+costeIncidente = linReg.predict(xNew)
+print("El coste del incidente sería:", int(costeIncidente[0]), "€")
+
+
+plt.plot(df['equiposAfectados'], df['costeIncidente'], "b.")
+plt.plot(xMinMax, yTrainPred, "g-")
+plt.plot(xNew, costeIncidente, "rx")
+plt.xlabel("Equipos afectados")
+plt.ylabel("Coste del incidente")
+plt.show()
---
+++ LogisticRegression.py
@@ -0,0 +1,71 @@
+from html.parser import HTMLParser
+import email
+import string
+import nltk
+nltk.download('stopwords')
+
+# Esta clase facilita el procesamiento de los correos electrónicos con código HTML
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+    
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+class Parser:
+    def __init__(self):
+        self.stemmer = nltk.PorterStemmer()
+        self.stopwords = set(nltk.corpus.stopwords.words('english'))
+        self.punctuation = list(string.punctuation)
+
+    def parse(self, email_path):
+        """Parse an email"""
+        with open(email_path, errors='ignore') as e:
+            msg = email.message_from_file(e)
+        return None if not msg else self.get_email_content(msg)
+    
+    def get_email_content(self, msg):
+        """Get the content of an email"""
+        subject = self.tokenize(msg['Subject']) if msg['Subject'] else []
+        body = self.get_email_body(msg.get_payload(), 
+                                   msg.get_content_type())
+        content_type = msg.get_content_type()
+        return {"subject": subject, 
+                "body": body, 
+                "content_type": content_type}
+    
+    def get_email_body(self, payload, content_type):
+        """Get the body of an email"""
+        body = []
+        if type(payload) is str and content_type == "text/plain":
+            return self.tokenize(payload)
+        elif type(payload) is str and content_type == "text/html":
+            return self.tokenize(strip_tags(payload))
+        elif type(payload) is list:
+            for p in payload:
+                body += self.get_email_body(p.get_payload(), 
+                                            p.get_content_type())
+        return body
+    
+    def tokenize(self, text):
+        """Tokenize a string"""
+        for c in self.punctuation:
+            text = text.replace(c, "")
+        text = text.replace("\t", " ")
+        text = text.replace("\n", " ")
+        tokens = list(filter(None, text.split(" ")))
+        return [self.stemmer.stem(w) for w in tokens if w not in self.stopwords]
+    
+inmail = open("../machineLearning-dataScience/datasets/trec07p/data/inmail.1").read()
+print(inmail)
